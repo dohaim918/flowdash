add7294ba0ad58ca2273f6dc80b8333c7d5f5bdd
kimsinjung189 <kimsinjung189@gamail.com>
2026-01-26 18:52:15 +0900

    feat : 조건검색 (우선순) 기능추가

--- js/fliter.js
+++ js/fliter.js
@@ -1,5 +1,5 @@
 // 여기에 드롭다운 JS 전체
-document.addEventListener('DOMContentLoaded', () => {
+
 
     // 요소 리스트 ( 안되면 점 ,"",절차 표시 다시확인 )
     // 첫번째 드롭다운 
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectedValueRanking = dropdownTriggerRanking.querySelector (".selected-value.ranking");
   const dropdownMenuRanking = document.querySelector (".dropdown-menu.ranking") // toggle 밖에 있음 (분리)
   const dropdownItemRanking = dropdownMenuRanking.querySelectorAll("li")
-
+ 
   // 주의 : 만약 ul li  버튼이 밖이면 document 헷갈리지말기..
 
   dropdownTrigger.addEventListener("click", (e) => {
     e.stopPropagation();
 
-    //  2번재 드롭다운 닫기
+    // 2번재 드롭다운 닫기
     dropdownToggleRanking.classList.remove("open");
     // 클릭한 드롭다운 열닫기! 
     dropdownToggle.classList.toggle("open");
@@ -36,20 +36,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 항목 클릭 → 글자 변경 + 메뉴 닫기
   // forEach : 모든 요소 하나씩~ (기능 추가하면 한개씩 기능)
-  // active 추가했어!!!
+  // 첫 번째 우선순위
   dropdownItem.forEach(item => {
     item.addEventListener("click", () => {
       // 전체 선택 해제부분
       dropdownItem.forEach(i => i.classList.remove("active"));
       item.classList.add("active");
       selectedValue.textContent = item.textContent;  // 텍스트 변경부분
-      // 클래스에 open 있으면 remove (제거) 해라~
+      // 클래스에 open 있으면 remove (제거) 
       dropdownToggle.classList.remove("open");
+      
     });
   });
 
-  // 항목 클릭 → 글자 변경 + 메뉴 닫기
-  // forEach : 모든 요소 하나씩~ (기능 추가하면 한개씩 기능)
+  // 두 번째 랭킹버튼
   dropdownItemRanking.forEach(item => {
     item.addEventListener("click", () => {
       selectedValueRanking.textContent = item.textContent;  // 텍스트 변경부분
@@ -64,61 +64,97 @@ document.addEventListener('DOMContentLoaded', () => {
        if (!dropdownToggle.contains(e.target)){ dropdownToggle.classList.remove("open")}
        if (!dropdownToggleRanking.contains(e.target) ){dropdownToggleRanking.classList.remove("open")}; 
     })
-})
+    
+    // ====================================== 전체 우선순 항목 ==============================================
+
+      dropdownItemRanking.forEach(item => {
+        item.addEventListener("click", () => {
+
+        // 클릭한 항목 텍스트
+        // 높음 / 중간 / 낮음 / 전체 우선순
+        const clickText = item.textContent.trim(); 
+
+        // 버튼 텍스트 변경 (버튼 안 텍스트)
+        selectedValueRanking.textContent = clickText;
+
+        // 카드 필터 부분 (출력부분)
+        const cardList = document.querySelectorAll(".task-card");
+
+        cardList.forEach(card => {
+          const tagEl = card.querySelector(".task-tag");
+          const tagText = tagEl ? tagEl.textContent.trim() : "";
+
+          // 버튼 안 || li안 텍스트 같아야지 나오게 아님 숨기기 
+          if (clickText === "전체 우선순" || tagText === clickText) {
+            card.style.display = "block";
+          } else {
+            card.style.display = "none";
+          }
+        });
+      });
+    });
+
+// ====================================== 전체 기간 항목 ==============================================
+
+
 
 //===============================================================================
 
-
-
-//================================================================================================
+//======================================================================================
 
 // 각 영역이 떨어져 있음 (검색 필터기능 부분)
 const cardSearch = document.querySelector(".card-search") // 검색 기능박스
-const cardList = Array.from(document.querySelectorAll(".task-card")); // 카드 리스트 
 const taskText = Array.from(document.querySelectorAll(".task-text")); // 제목 + 내용 (박스)
 // const taskDesc = Array.from(document.querySelectorAll(".task-desc"));
 
 
 cardSearch.addEventListener("input", () => {
-  // 검색창 소문자로도 검색하게 끔
+  const cardList = Array.from(document.querySelectorAll(".task-card")); // 카드 리스트 
+  // 변형 되는 부분 / todos 배열 가져오기 (로컬스토리지)
+  let todos = JSON.parse(localStorage.getItem("flowdash-todos")) || [];
   const keyText = cardSearch.value.toLowerCase();
-  const filteredTodos = []; // 로컬스토리지 저장할 배열
-
-  // 카드 리스트 검사
+  
+  // 카드 리스트 검사 (화면 렌더링부분임)
   cardList.forEach(card => {
-  // 중간 배찌도 검색함...
+  // 중간 배찌도 검색해서 특정요소 뽑아서 
   const cardTextEl = card.querySelector(".task-text"); // 제목 + 내용만
-  const cardText = cardTextEl.innerText.toLowerCase(); // 소문자 검색가능
-  //  문자열 안에 특정 글자 있는가 확인(includes)
+  const cardText = cardTextEl ? cardTextEl.innerText.toLowerCase().trim() : ""; // 소문자+공백 포함
+  //  제목+내용+소문자(toL...)+공배(trim) 포함 특정 키 있나 확인(includes)
   if (cardText.includes(keyText)) {
       card.style.display = "block";
-      // 배열 객체 추가
-      filteredTodos.push({
-        id: card.dataset.id,
-        text: cardTextEl.innerText
-      });
     } else {
       card.style.display = "none";
     }
 
+  })
 })
-  // 로컬 
-  localStorage.setItem("flowdash-todos", JSON.stringify(filteredTodos));
-})
+
 //=====================================================================
 
 //=====================================================================
 // 전체 초기화 버튼! 
 const allDelete = document.querySelector(".all-delete");    
-const todosList = document.querySelector(".task-list");
+const todosList = document.querySelectorAll(".task-list");
 
 // 기존 flowdash-todos 배열 가져오는데 || 값이 없으면 빈 배열!!
 let todos = JSON.parse(localStorage.getItem("flowdash-todos")) || [];
 
 allDelete.addEventListener("click", () => {
-    todos = [];  // 배열 초기화
-    
-    // 로컬 삭제(key 정확히)작성..
-    localStorage.removeItem("flowdash-todos") 
-    if(todosList) todosList.innerHTML = ""; // 화면부분 
+  todos.splice(0, todos.length); 
+
+    // 로컬 스토리지 빈 배열로 
+    localStorage.setItem("flowdash-todos", JSON.stringify(todos));
+
+    // 화면 부분 초기화!
+     todosList.forEach(list => {
+      list.innerHTML = "";
+    });
 });
+
+
+
+// ====================================================================
+
+
+
+    
